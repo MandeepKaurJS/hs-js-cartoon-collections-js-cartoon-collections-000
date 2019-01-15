63dfd81fd39c5d2410908fb4478ba9abaf77e069
Mandeep Kaur <vjsing445@gmail.com>
2019-01-15 17:47:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+  var arreyfun1=[];
+  for(var i=0;i<dwarves.length;i++){
+    arreyfun1.push(i+1+". "+dwarves[i]+" ");
+  }
+  return arreyfun1.join('');
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var calls=[];
+  for(var i=0;i<planeteerCalls.length;i++){
+     calls.push(planeteerCalls[i].toUpperCase()+"!");
+ }
+ return calls
 }
-
 function longPlaneteerCalls(words) {
+  for(var i=0;i<words.length;i++){
+    if(words[i].length>4)
+    {
+      return true
+    }
+    return false
+  }
 }
 
 function findTheCheese (foods) {
+  var newarray=["camembert","cheddar","gouda"];
+  for(var i=0;i<foods.length;i++){
+    var cheeseindex=newarray.indexOf(foods[i]);
+    if(cheeseindex!==-1)
+    return foods[i];
+  } 
+  return "no cheese!";
 }
