a2cb84d306612e99d274409b4a928c4b3946b3da
Mandeep Kaur <vjsing445@gmail.com>
2019-01-28 18:46:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function findTheCheese (foods) {
   for(var i=0;i<foods.length;i++){
     var cheeseindex=newarray.indexOf(foods[i]);
     for(var j=0;j<newarray.length;j++){
-      empty.push(newarray[j].foods[i]);
+      empty.push(foods[i]);
     }
     return empty
     if(cheeseindex!==-1)
